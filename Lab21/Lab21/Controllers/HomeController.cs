7ac8d0b0f45466902eb2c37b960c9d9952871c9b
Jason <jassle@att.net>
2018-05-29 01:34:55 -0400

    first commit

---
+++ Lab21/Lab21/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using Lab21.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab21.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        } 
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Register()
+        {
+
+            return View();
+        }
+        public ActionResult AddNewUser(UserInfo newuser)
+        {
+            //Validation
+            if (ModelState.IsValid)
+            {
+                //insert newUser into the DB
+                ViewBag.Message = $"Thank you for registering {newuser.FirstName}";
+                return View("confirm");
+            }
+            else
+            {
+                ViewBag.Address = Request.UserHostAddress;
+                return View("Error");
+            }
+        }
+    }
+}
